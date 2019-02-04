f434145ec85f0566a01948e9731662ceaf43694f
alexgimson86 <alexgimson@gmail.com>
2019-02-04 17:47:44 -0600

    added star images for rating systenm

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ class App extends Component {
     movies.forEach((movie) =>{
     const movieRow =(
       <div key={movie.id}>
-        <img alt="movie poster" src={movie.poster_src}></img>
-        {movie.title}
-        {movie.overview}
+        <div className="jumbotron">
+          <h1 className="display-4">{movie.title}</h1>
+          <img alt="movie poster" src={movie.poster_src}></img>
+          <p>{movie.overview}</p>
+      </div>
      </div> )
      movieRows.push(movieRow);
     })
+  
     this.state = {
       rows: movieRows,
       movieName: ''
@@ -27,26 +30,45 @@ class App extends Component {
   handleChange = (e) => {
     var movieRows = [];
     this.setState({[e.target.name]: e.target.value })
-    $.ajax({
-      url:`https://api.themoviedb.org/3/search/movie?api_key=baf70dcc60ac4c339602a8f30a38501d&language=en-US&query=${e.target.value}&page=1&include_adult=true`
-      
+    var starArr = []
+    let urlString = `https://api.themoviedb.org/3/search/movie?api_key=baf70dcc60ac4c339602a8f30a38501d&language=en-US&query=${e.target.value}&page=1&include_adult=true`
+    fetch(urlString)
+    .then(response => response.json())
+    .then((movies)=>{
+
+      if(movies.results){
+        movies.results.forEach((movie)=>{
+           
+            let s = "https://image.tmdb.org/t/p/w185/"
+            s += movie.poster_path
+            let avg = Math.floor(movie.vote_average)
+            let diff = movie.vote_average - avg
+            for(let index = 0; index < avg; index++){
+              starArr.push(<img src="../star.jpeg" alt="star" width="150" height="100"/>)
+            }
+            if(diff >= .5){
+              starArr.push(<img src="../halfstar.jpg" alt="star" width="150" height="100"/>)
+            }
+
+            const movieRow = (<div key={movie.id}>
+          <div className="jumbotron">
+            <h1 className="display-4"><b>{movie.title}</b></h1><br/>
+            </div>
+            <img alt="movie poster" src={s}></img>
+          <h3>Overview: </h3> <p>{movie.overview}</p>
+          <h3>Vote average: </h3><p>{movie.vote_average} out of 10 stars</p>
+          
+          {starArr.map((star, i) => (
+            <div className="landscape" key={i}>{star}</div>
+          ))}
+          <hr></hr>
+        </div>)
+        movieRows.push(movieRow)
+        starArr = []
+        })
+        this.setState({rows: movieRows})
+      }
     })
-  .done(function(movies) {
-    movies.results.forEach((movie)=>{
-      let s = "https://image.tmdb.org/t/p/w185/"
-      s += movie.poster_path
-      let avg = Math.floor(movies.vote_average)
-      const movieRow = (<div key={movie.id}>
-          <img alt="movie poster" src={s}></img>
-        <h3>Title: </h3><h1><b>{movie.title}</b></h1><br/>
-         <h3>Overview: </h3> <p>{movie.overview}</p>
-         <h3>Vote average: </h3><p>{movie.vote_average} out of 10 stars</p>
-         <hr></hr>
-      </div>)
-      movieRows.push(movieRow)
-    })
-    this.setState({rows: movieRows})
-  }.bind(this))
 }
     
   
